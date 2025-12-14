357243a10f78c320259f780991dcfe25ec1e2f96
Ricardo Chavez <gic.rc455@gmail.com>
2025-12-14 12:23:50 -0600

    Comentar la llamada a sync2 en main y asegurar la ejecución de sync3

--- bin/02_funciones/async.dart
+++ bin/02_funciones/async.dart
@@ -1,7 +1,8 @@
 
 void main() {
   sync1();
-  sync2();
+  //sync2();
+  sync3();
 
 }
 
@@ -15,4 +16,18 @@ Future<void> sync1()async{
 void sync2() {
   print('Tarea 2'); 
 
+}
+
+void sync4() {
+  for  (int i = 10; i >= 1; i--) {
+    print(i);
+
+  }
+}
+
+Future<void> sync3() async {
+  for  (int i = 10; i >= 1; i--) {
+    print(i);
+    await Future.delayed(Duration(seconds: 1));
+  }
 }
